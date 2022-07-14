0f12ca8ff62575edd9f4d04fdba5480e56abe834
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-14 14:24:03 +0530

    Gambler: First Commit

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Gambler {
+
+    static final int INITIAL_STAKE=100;
+
+    public static void main(String[] args) {
+        System.out.println("Initial Stake = $"+INITIAL_STAKE);
+
+    }
+}
