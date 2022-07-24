cfea51ad7c06260695c99300beff15f6ca6b7054
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-24 12:46:07 +0530

    Initial Stake

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -2,10 +2,14 @@ package com.bridgelabz;
 import java.util.*;
 public class Gambler {
 
-    static final int INITIAL_STAKE=100;
+    static int INITIAL_STAKE=100;
     static int BET_AMOUNT=1;
+    static int win = 1;
+
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator\n");
+
         System.out.println("Initial Amount = $"+INITIAL_STAKE);
         System.out.println("Bet Amount = $"+BET_AMOUNT);
 
